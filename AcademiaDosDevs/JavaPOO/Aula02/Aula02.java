2c4c75d7409939f13db8d4070bf826a269192837
MrZeroLeft <77200265+MrZeroLeft@users.noreply.github.com>
2022-04-06 17:14:13 -0300

    Add files via upload

--- AcademiaDosDevs/JavaPOO/Aula02/Aula02.java
+++ AcademiaDosDevs/JavaPOO/Aula02/Aula02.java
@@ -9,15 +9,12 @@ public class Aula02 {
         celular1.tamanhoTela = 6.1f;
 
         Celular celular2 = new Celular(); // declarando objeto do tipo Celular
-        celular1.nome = "Galaxy Note 20 Ultra";
-        celular1.sistemaOperacional = "Android";
-        celular1.espacoArmazenamento = 256;
-        celular1.tamanhoTela = 6.9f;
+        celular2.nome = "Galaxy Note 20 Ultra";
+        celular2.sistemaOperacional = "Android";
+        celular2.espacoArmazenamento = 256;
+        celular2.tamanhoTela = 6.9f;
 
-        System.out.format("O celular %s tem o sistema operacional %s, com espaço de memória de %d gb e uma tela de %.1f polegadas \n \n",
-        celular1.nome, celular1.sistemaOperacional, celular1.espacoArmazenamento, celular1.tamanhoTela);
-
-        System.out.format("O celular %s tem o sistema operacional %s, com espaço de memória de %d gb e uma tela de %.1f polegadas \n \n",
-        celular2.nome, celular2.sistemaOperacional, celular2.espacoArmazenamento, celular2.tamanhoTela);
+       celular1.qualidadesCelular();
+       celular2.qualidadesCelular();
     }
 }
--- AcademiaDosDevs/JavaPOO/Aula02/Celular.java
+++ AcademiaDosDevs/JavaPOO/Aula02/Celular.java
@@ -7,4 +7,18 @@ public class Celular {
     int espacoArmazenamento; // Criando um atributo do tipo int e chamado nome para a classe Celular
     float tamanhoTela; // Criando um atributo do tipo float e chamado tamanhoTela para a classe Celular
     
+    void qualidadesCelular() {
+        System.out.format("O celular %s tem o sistema operacional %s, com espaço de memória de %d gb e uma tela de %.1f polegadas \n\n",
+        nome, sistemaOperacional, espacoArmazenamento, tamanhoTela);
+    };
+
+    void onOff() {
+        boolean on = true;
+        boolean off = false;
+
+        if(on) {
+
+        }
+        
+    }
 }
